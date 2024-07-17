3f126265d6c07cfcd52149cd82e520f74535ced1
Hyowon Shin <4llll1019@gmail.com>
2024-07-17 19:20:09 +0900

    third commit

--- src/main/java/com/hws/excelmaker/controller/ExcelMakerController.java
+++ src/main/java/com/hws/excelmaker/controller/ExcelMakerController.java
@@ -7,60 +7,53 @@ import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import java.io.FileOutputStream;
 import java.io.IOException;
 
 @Controller
+@RequestMapping("/excelmaker")
 public class ExcelMakerController {
 
-    @PostMapping("/checker")
-    public String inputDataChecker(Model model, @RequestParam("contractDate") String contactDate, @RequestParam("customerName") String customerName, @RequestParam("belong") String belong, @RequestParam("carName") String carName,
+    @GetMapping
+    public String addForm() {
+        return "excelmaker/addForm";
+    }
+
+    @PostMapping("/check")
+    public String inputDataChecker(Model model, @RequestParam("contractDate") String contractDate, @RequestParam("customerName") String customerName, @RequestParam("belong") String belong, @RequestParam("carName") String carName,
                                    @RequestParam("carPrice") Integer carPrice, @RequestParam("releaseStore") String releaseStore, @RequestParam("charge") Integer charge, @RequestParam("progress") String progress,
                                    @RequestParam("cashBack") Integer cashBack, @RequestParam("releasePlace") String releasePlace, @RequestParam("supportContents") String supportContents, @RequestParam("etcContents") String etcContents,
                                    @RequestParam("enrollDate") String enrollDate, @RequestParam("carNumber") String carNumber) throws IOException {
-//        Workbook workBook = new XSSFWorkbook();
-//
-//        Sheet sheet0 = workBook.createSheet("테스트");
-//
-//        Row row0 = sheet0.createRow(0);
-//
-//        row0.createCell(0).setCellValue(contactDate);
-//        row0.createCell(1).setCellValue(customerName);
-//        row0.createCell(2).setCellValue(belong);
-//
-//        //@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
-//        // 파일 덮어써지지 않도록 보완하기(새로 생성하는 것이 아닌 이미 해당 이름의 파일이 있다면, 불러와서 아래 행부터 추가하는 것으로 수정)
-//        //@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
-//
-//        FileOutputStream fos = new FileOutputStream("C:\\myDev\\mine.xlsx");
-//        workBook.write(fos);
-//        fos.close();
-//        workBook.close();
 
-        ExcelData inputData = new ExcelData(contactDate, customerName, belong, carName, carPrice, releaseStore, charge, progress, cashBack, releasePlace, supportContents, etcContents, enrollDate,carNumber);
+        ExcelData inputData = new ExcelData(contractDate, customerName, belong, carName, carPrice,
+                releaseStore, charge, progress, cashBack, releasePlace, supportContents, etcContents, enrollDate,carNumber);
 
         model.addAttribute("inputData", inputData);
 
 
-        return "checker/check";
+        return "excelmaker/check";
     }
 
-    @PostMapping("/excelMake")
-    @ResponseBody
-    public String excelMaker(@RequestParam("contractDate") String contractDate, @RequestParam("customerName") String customerName, @RequestParam("belong") String belong,
+    @PostMapping("/make")
+    public String excelMaker(Model model, @RequestParam("contractDate") String contractDate, @RequestParam("customerName") String customerName, @RequestParam("belong") String belong,
                              @RequestParam("carName") String carName, @RequestParam("carPrice") Integer carPrice, @RequestParam("releaseStore") String releaseStore,
                              @RequestParam("charge") Integer charge, @RequestParam("progress") String progress, @RequestParam("cashBack") Integer cashBack,
                              @RequestParam("releasePlace") String releasePlace, @RequestParam("supportContents") String supportContents, @RequestParam("etcContents") String etcContents,
                              @RequestParam("enrollDate") String enrollDate, @RequestParam("carNumber") String carNumber) throws IOException {
+        ExcelData inputData = new ExcelData(contractDate, customerName, belong, carName, carPrice,
+                releaseStore, charge, progress, cashBack, releasePlace, supportContents, etcContents, enrollDate,carNumber);
+
+        model.addAttribute("inputData", inputData);
+
         Workbook workBook = new XSSFWorkbook();
 
-        Sheet sheet0 = workBook.createSheet("테스트");
+        Sheet sheet0 = workBook.createSheet("고객정보");
 
         Row row0 = sheet0.createRow(0);
+        // Integer myRow = sheet0.getLastRowNum(); -> @@@@@@@@@@@@@@@@@@@@@@@@@이런식으로 마지막 행 얻기 (마지막 열 얻는 방법도 같음.)
+        // PRG 처리하기.
 
         row0.createCell(0).setCellValue(contractDate);
         row0.createCell(1).setCellValue(customerName);
@@ -79,6 +72,7 @@ public class ExcelMakerController {
 
         //@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
         // 파일 덮어써지지 않도록 보완하기(새로 생성하는 것이 아닌 이미 해당 이름의 파일이 있다면, 불러와서 아래 행부터 추가하는 것으로 수정)
+        // 같은 POST로 요청시에 PRG관련하여 오류 막을 것.
         //@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
 
         FileOutputStream fos = new FileOutputStream("C:\\myDev\\mine.xlsx");
@@ -86,6 +80,7 @@ public class ExcelMakerController {
         fos.close();
         workBook.close();
 
-        return "엑셀 생성 완료";
+        return "excelmaker/result";
     }
+
 }
