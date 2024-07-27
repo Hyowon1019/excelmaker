1c963c887830ca2d0c4d31ebd35065c0004b4568
Hyowon Shin <4llll1019@gmail.com>
2024-07-27 15:12:32 +0900

    feat(ExcelMakerController): modify make() feat(index.html): add button with javascript method style(index.html): modify position of button refactor(addForm.html) refactor(check.html) refactor(index.html) refactor(ExcelMakerController): change name of mapping URL

--- src/main/java/com/hws/excelmaker/controller/ExcelMakerController.java
+++ src/main/java/com/hws/excelmaker/controller/ExcelMakerController.java
@@ -16,7 +16,7 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 
 @Controller
-@RequestMapping("/excelmaker")
+@RequestMapping("/excel-maker")
 @RequiredArgsConstructor
 public class ExcelMakerController {
 
@@ -26,7 +26,7 @@ public class ExcelMakerController {
 
     @GetMapping
     public String addForm() {
-        return "excelmaker/addForm";
+        return "excel-maker/addForm";
     }
 
     @PostMapping("/check")
@@ -35,13 +35,12 @@ public class ExcelMakerController {
                                    @RequestParam("cashBack") Integer cashBack, @RequestParam("releasePlace") String releasePlace, @RequestParam("supportContents") String supportContents, @RequestParam("etcContents") String etcContents,
                                    @RequestParam("enrollDate") String enrollDate, @RequestParam("carNumber") String carNumber) throws IOException {
 
-        ExcelData inputData = new ExcelData(contractDate, customerName, belong, carName, carPrice,
-                releaseStore, charge, progress, cashBack, releasePlace, supportContents, etcContents, enrollDate,carNumber);
+        ExcelData inputData = new ExcelData(contractDate, customerName, belong, carName, carPrice, releaseStore, charge, progress, cashBack, releasePlace, supportContents, etcContents, enrollDate,carNumber);
 
         model.addAttribute("inputData", inputData);
 
 
-        return "excelmaker/check";
+        return "excel-maker/check";
     }
 
     @PostMapping("/make")
@@ -50,18 +49,11 @@ public class ExcelMakerController {
                              @RequestParam("charge") Integer charge, @RequestParam("progress") String progress, @RequestParam("cashBack") Integer cashBack,
                              @RequestParam("releasePlace") String releasePlace, @RequestParam("supportContents") String supportContents, @RequestParam("etcContents") String etcContents,
                              @RequestParam("enrollDate") String enrollDate, @RequestParam("carNumber") String carNumber) throws IOException {
-        ExcelData inputData = new ExcelData(contractDate, customerName, belong, carName, carPrice,
-                releaseStore, charge, progress, cashBack, releasePlace, supportContents, etcContents, enrollDate,carNumber);
+
+        ExcelData inputData = new ExcelData(contractDate, customerName, belong, carName, carPrice, releaseStore, charge, progress, cashBack, releasePlace, supportContents, etcContents, enrollDate,carNumber);
 
         model.addAttribute("inputData", inputData);
 
-        // if 문 활용하여 만약 기존 파일이 있으면 이어서 작성, 없으면 새로 생성하여 작성(새로하는건 이미 아래와 같이 만들어져 있음) Integer myRow = sheet0.getLastRowNum(); -> @@@@@@@@@@@@@@@@@@@@@@@@@이런식으로 마지막 행 얻기 (마지막 열 얻는 방법도 같음.)
-        //validation 구현
-        // PRG 처리하기.
-        //@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
-        // 파일 덮어써지지 않도록 보완하기(새로 생성하는 것이 아닌 이미 해당 이름의 파일이 있다면, 불러와서 아래 행부터 추가하는 것으로 수정)
-        // 같은 POST로 요청시에 PRG관련하여 오류 막을 것.
-        //@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
         String filePath = "C:\\myDev\\mine.xlsx";
         File file = new File(filePath);
 
@@ -71,7 +63,8 @@ public class ExcelMakerController {
             excelMakerService.createFile(inputData, filePath);
         }
 
-        return "excelmaker/result";
+        //새로고침으로 인한 POST 중복 요청을 방지하기 위한 PRG 처리
+        return "redirect:/";
     }
 
 }
